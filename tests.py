c85d396eff999f14f6d106e04f60108c570dfb27
Ray Kiddy <ray@ganymede.org>
2022-11-27 14:55:33 -0800

    Start adding tests.

--- tests.py
+++ tests.py
@@ -4,15 +4,53 @@ import data
 
 def test_year_value_for_contract():
 
+    # this should be 6 days that evenly cross a year boundary.
     c = {
-        'effective_date': '2022-07-25',
-        'expir_date': '2023-06-30',
-        'contract_value': 58250000
+        'effective_date': '2022-12-29',
+        'expir_date': '2023-01-03',
+        'contract_value': 1000
     }
 
-    result = data.year_value_for_contract(c)
-    print(f"result: {result}")
+    assert data.year_value_for_contract(c, '2022') == 500
+    assert data.year_value_for_contract(c, '2023') == 500
+
+    c = {
+        'effective_date': '2022-12-01',
+        'expir_date': '2022-12-10',
+        'contract_value': 1000
+    }
+
+    assert data.year_value_for_contract(c, '2022') == 1000
+    assert data.year_value_for_contract(c, '2023') == 0
+    assert data.year_value_for_contract(c, '2021') == 0
+
+    c = {
+        'effective_date': '2022-01-01',
+        'expir_date': '2022-12-31',
+        'contract_value': 1000
+    }
+
+    assert data.year_value_for_contract(c, '2022') == 1000
+    assert data.year_value_for_contract(c, '2023') == 0
+    assert data.year_value_for_contract(c, '2021') == 0
+
+    c = {
+        'effective_date': '2021-12-30',
+        'expir_date': '2023-01-02',
+        'contract_value': 36900
+    }
+
+    assert data.year_value_for_contract(c, '2022') == 36500
 
 
-if __name__ == '__main__':
-    test_year_value_for_contract()
+def test_money():
+
+    assert data.money(100) == '$ 1.00'
+    assert data.money(100000) == '$ 1,000.00'
+    assert data.money(123456) == '$ 1,234.56'
+
+
+def test_cost_labels():
+
+    assert data.cost_labels(0) == 'B0'
+    assert data.cost_labels(100000000) == 'B9'
